6b70343e9f6702d4a1f02e9a5a5310dd55443803
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-01-27 16:34:25 +0200

    Maked own server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+// const crypto = require('crypto');
 //Libuv - это многопоточная библиотека которая предназначена для переработки 
 //в машинный код того что выдает ядро Node 
 //оно работает на движке v8 и обрабатывает JS код
@@ -30,3 +30,45 @@ const crypto = require('crypto');
 // crypto.pbkdf2('123ttt', '5', 1000000, 64, 'sha512', () => {
 //     console.log('6 start', Date.now() - start);
 // });
+
+const http = require('http');
+
+//для инициализации переменных в файле .env
+require('dotenv').config();
+
+const PORT = process.env.PORT || '3000';
+
+const server = http.createServer((req, res) => {
+    //для того чтобы браузер прочитал кирилицу нужно указать объект с заголовками настроек
+    // res.writeHead(200, {
+    //     'Content-type': 'text/html; charset="utf8"'
+    // });
+    //метод отправки данных пользователю для закрития стрима
+    //можно использовать для серверсайд рендеримнга
+    // res.end('<h1>Hello from server!!!<button>Click Me</button></h1>');
+
+    //req.url
+    //содержит в себе состояние сторки запроса после слеша
+
+    //для работы с JSON
+    res.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    if (req.url === '/users'){
+        return res.end(JSON.stringify([
+            {id: 1, name: 'Eugene'}
+        ]));
+    };
+
+    if (req.url === '/posts'){
+        return res.end(JSON.stringify([
+            {id: 1, postText: 'My first post'}
+        ]));
+    };
+
+    res.end(req.url);
+});
+
+//функция для прослушиввания входящих соединений
+server.listen(PORT, () => console.log(`Server started on PORT=${PORT}`));
